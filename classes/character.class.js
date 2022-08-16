9d3b1e3e51477310ad8227a84d2b168ec1325fdb
Tobias Fernkorn <fernsikos@gmail.com>
2022-08-16 17:05:41 +0200

    first stuck

---
+++ classes/character.class.js
@@ -0,0 +1,13 @@
+class Character extends Moveableobject {
+
+    constructor() {
+        super();
+        this.loadImage('img/2_character_pepe/1_idle/idle/I-1.png')
+    }
+
+
+
+    jump() {
+
+    }
+}
---
+++ classes/movable-object.class.js
@@ -0,0 +1,22 @@
+class Moveableobject {
+    x = 100;
+    y = 100;
+    height = 150;
+    width = 100;
+    img;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight() {
+
+    }
+
+    moveLeft() {
+
+    }
+
+
+}
