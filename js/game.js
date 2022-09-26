a912795a7ff5b335f71c931c78840c1567063fff
Tobias Fernkorn <fernsikos@gmail.com>
2022-09-26 15:34:19 +0700

    test

--- js/game.js
+++ js/game.js
@@ -19,6 +19,33 @@ function init() {
 //     }, 2000);
 // }
 
+function fullscreen() {
+    let fullscreen = document.getElementById('fullscreen');
+    openFullscreen(fullscreen);
+}
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+  
+  /* Close fullscreen */
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+  }
+
 window.addEventListener('keydown', (event) => {
     if (event.keyCode == 37) {
         keyboard.LEFT = true;
