4eb257279e69c114b90e58b63b23080bf4267984
Tobias Fernkorn <fernsikos@gmail.com>
2022-09-29 15:52:21 +0700

    problem touch event

--- classes/keyboard.class.js
+++ classes/keyboard.class.js
@@ -5,4 +5,74 @@ class Keyboard {
     RIGHT;
     SPACE;
     D;
+
+    constructor() {
+        this.keyboardEvents();
+        // this.touchEvents();
+    }
+
+    touchEvents() {
+        document.getElementById('btn-up').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.UP = true;
+        });
+        
+        document.getElementById('btn-up').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.UP = false;
+        });
+    }
+
+    keyboardEvents() {
+        window.addEventListener('keydown', (event) => {
+            if (event.keyCode == 37) {
+                keyboard.LEFT = true;
+            }
+        
+            if (event.keyCode == 38) {
+                keyboard.UP = true;
+            }
+        
+            if (event.keyCode == 39) {
+                keyboard.RIGHT = true;
+            }
+        
+            if (event.keyCode == 40) {
+                keyboard.DOWN = true;
+            }
+        
+            if (event.keyCode == 32) {
+                keyboard.SPACE = true;
+            }
+            if (event.keyCode == 68) {
+                keyboard.D = true;
+            }
+        });
+
+        window.addEventListener('keyup', (event) => {
+            if (event.keyCode == 37) {
+                keyboard.LEFT = false;
+            }
+        
+            if (event.keyCode == 38) {
+                keyboard.UP = false;
+            }
+        
+            if (event.keyCode == 39) {
+                keyboard.RIGHT = false;
+            }
+        
+            if (event.keyCode == 40) {
+                keyboard.DOWN = false;
+            }
+        
+            if (event.keyCode == 32) {
+                keyboard.SPACE = false;
+            }
+        
+            if (event.keyCode == 68) {
+                keyboard.D = false;
+            }
+        })
+    }
 }
